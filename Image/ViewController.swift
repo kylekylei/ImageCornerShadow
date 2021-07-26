e6d423c63222caef4df231baab2afd2e20beed5b
Kyle <kylei.tw@gmail.ocm>
2021-07-26 12:03:29 +0800

    The method to let image has corner radius and shadow

--- Image/ViewController.swift
+++ Image/ViewController.swift
@@ -17,3 +17,24 @@ class ViewController: UIViewController {
 
 }
 
+
+class MainUIView: UIView {
+    @IBOutlet var containerView: UIView!{
+        didSet{
+            containerView.layer.shadowOffset = CGSize(width: 0,height: 24)
+            containerView.layer.shadowRadius = 20
+            containerView.layer.shadowOpacity = 1
+            containerView.layer.shadowColor = CGColor(srgbRed: 1/255, green: 29/255, blue: 103/255, alpha: 0.2)
+        }
+    }
+    @IBOutlet var heroImage: UIImageView! {
+     didSet{
+        heroImage.clipsToBounds = true
+        heroImage.layer.opacity = 0.87
+        heroImage.layer.cornerRadius = 40
+        heroImage.layer.borderWidth = 2
+        heroImage.layer.borderColor = CGColor(srgbRed: 1/255, green: 29/255, blue: 103/255, alpha: 0.5)
+        
+     }
+    }
+}
